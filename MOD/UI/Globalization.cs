a757895b6a17ff4cf124e89e122682ba04b2efbe
gansaku <negi4gn@gmail.com>
2017-08-14 08:20:09 +0900

    ver.3.4.1

--- MOD/UI/Globalization.cs
+++ MOD/UI/Globalization.cs
@@ -9,42 +9,45 @@ namespace CSLMod.CSLMapView.Modding.UI {
     /// </summary>
     internal class Globalization {
         internal const string DEFAULT_LANGUAGE = "en";
-        private readonly string[] supportedLanguages = new[] { "en","ja","zh","zh-tw" };
-        private Dictionary<string,Dictionary<string, string>> strings;
+        private readonly string[] supportedLanguages = new[] { "en", "ja", "zh", "zh-tw" };
+        private Dictionary<string, Dictionary<StringKeys, string>> strings;
         /// <summary>
         /// 文字列のキー
         /// </summary>
-        internal static class Keys {
+        internal enum StringKeys {
             /// <summary>
             /// エクスポート
             /// </summary>
-            internal const string Export = "Export";
+            Export,
             /// <summary>
             /// エクスポートしてビューアを起動
             /// </summary>
-            internal const string ExportAndRun = "ExportAndRun";
+            ExportAndRun,
             /// <summary>
             /// 説明ラベル1のテキスト
             /// </summary>
-            internal const string DescLabel1 = "DescLabel1";
+            DescLabelExeOutputPath,
+            DescLabelCslmapOutputPath,
             /// <summary>
             /// エクスポートに失敗しました。
             /// </summary>
-            internal const string FailedToExportMap = "FailedToExportMap";
+            FailedToExportMap,
             /// <summary>
             /// ファイルのコピーに失敗しました。
             /// </summary>
-            internal const string FailedToCopyFile = "FailedToCopyFile";
+            FailedToCopyFile,
             /// <summary>
             /// ゲームを開始してから実行してください。
             /// </summary>
-            internal const string NotInGame = "NotInGame";
+            NotInGame,
             /// <summary>
             /// exeの起動に失敗しました。
             /// </summary>
-            internal const string FailedToRunViewer = "FailedToRunViewer";
+            FailedToRunViewer,
 
-            internal const string Compress = "Compress";
+            Compress,
+
+            AddTimestampToFileName
         }
         /// <summary>
         /// 現在の言語
@@ -53,54 +56,59 @@ namespace CSLMod.CSLMapView.Modding.UI {
         /// <summary>
         /// コンストラクタ
         /// </summary>
-        internal Globalization( ) {
-            strings = new Dictionary<string, Dictionary<string, string>>();
-            
-            strings.Add( "en", new Dictionary<string, string>() );
+        internal Globalization() {
+            strings = new Dictionary<string, Dictionary<StringKeys, string>>();
+
+            strings.Add( "en", new Dictionary<StringKeys, string>() );
             initEn( strings["en"] );
-            strings.Add( "ja", new Dictionary<string, string>() );
+            strings.Add( "ja", new Dictionary<StringKeys, string>() );
             initJa( strings["ja"] );
-            strings.Add( "zh", new Dictionary<string, string>() );
+            strings.Add( "zh", new Dictionary<StringKeys, string>() );
             initZh( strings["zh"] );
-            strings.Add( "zh-tw", new Dictionary<string, string>() );
+            strings.Add( "zh-tw", new Dictionary<StringKeys, string>() );
             initZh( strings["zh-tw"] );
         }
-        private void initJa(Dictionary<string,string> dic) {
-            dic.Add( Keys.Export, "エクスポート" );
-            dic.Add( Keys.ExportAndRun, "エクスポートしてビューアを起動" );
-            dic.Add( Keys.DescLabel1, "マップファイル(*.cslmap)とビューアの出力先：" );
-            dic.Add( Keys.FailedToExportMap, "エクスポートに失敗しました。" );
-            dic.Add( Keys.FailedToCopyFile, "ファイルのコピーに失敗しました。" );
-            dic.Add( Keys.NotInGame, "ゲームを開始してから実行してください。" );
-            dic.Add( Keys.FailedToRunViewer, $"{CSLMapView.VIEWER_EXE}の起動に失敗しました。" );
-            dic.Add( Keys.Compress, ".cslmapファイルの圧縮 (*.cslmap.gzを出力します)" );
+        private void initJa( Dictionary<StringKeys, string> dic ) {
+            dic.Add( StringKeys.Export, "エクスポート" );
+            dic.Add( StringKeys.ExportAndRun, "エクスポートしてビューアを起動" );
+            dic.Add( StringKeys.DescLabelExeOutputPath, "ビューアのパス(自動的にインストールされます)" );
+            dic.Add( StringKeys.DescLabelCslmapOutputPath, "マップファイル(*.cslmap)の出力先" );
+            dic.Add( StringKeys.FailedToExportMap, "エクスポートに失敗しました。" );
+            dic.Add( StringKeys.FailedToCopyFile, "ファイルのコピーに失敗しました。" );
+            dic.Add( StringKeys.NotInGame, "ゲームを開始してから実行してください。" );
+            dic.Add( StringKeys.FailedToRunViewer, $"{CSLMapView.VIEWER_EXE}の起動に失敗しました。" );
+            dic.Add( StringKeys.Compress, ".cslmapファイルの圧縮 (*.cslmap.gzを出力します)" );
+            dic.Add( StringKeys.AddTimestampToFileName, "ファイル名にタイムススタンプを付与" );
         }
-        private void initEn(Dictionary<string,string> dic ) {
-            dic.Add( Keys.Export, "Export" );
-            dic.Add( Keys.ExportAndRun, "Export and Run viewer" );
-            dic.Add( Keys.DescLabel1, "Map file(*.cslmap) and the viewer will be copied to" );
-            dic.Add( Keys.FailedToExportMap, "Failed to export map." );
-            dic.Add( Keys.FailedToCopyFile, "Failed to copy files." );
-            dic.Add( Keys.NotInGame, "Map is not loaded." );
-            dic.Add( Keys.FailedToRunViewer, $"Failed to run {CSLMapView.VIEWER_EXE}" );
-            dic.Add( Keys.Compress, "Compress .cslmap (outputs *.cslmap.gz)" );
+        private void initEn( Dictionary<StringKeys, string> dic ) {
+            dic.Add( StringKeys.Export, "Export" );
+            dic.Add( StringKeys.ExportAndRun, "Export and Run viewer" );
+            dic.Add( StringKeys.DescLabelExeOutputPath, "Viewer app path (will be automatically installed)" );
+            dic.Add( StringKeys.DescLabelCslmapOutputPath, "Map file(*.cslmap) output path" );
+            dic.Add( StringKeys.FailedToExportMap, "Failed to export map." );
+            dic.Add( StringKeys.FailedToCopyFile, "Failed to copy files." );
+            dic.Add( StringKeys.NotInGame, "Map is not loaded." );
+            dic.Add( StringKeys.FailedToRunViewer, $"Failed to run {CSLMapView.VIEWER_EXE}" );
+            dic.Add( StringKeys.Compress, "Compress .cslmap (outputs *.cslmap.gz)" );
+            dic.Add( StringKeys.AddTimestampToFileName, "Add timestamp to file name" );
         }
-        private void initZh( Dictionary<string, string> dic ) {
-            dic.Add( Keys.Export, "輸出" );
-            dic.Add( Keys.ExportAndRun, "輸出后開啟" );
-            dic.Add( Keys.DescLabel1, "地圖文件（*.cslmap）以及檢視器程式將會複製到以下路徑" );
-            dic.Add( Keys.FailedToExportMap, "輸出失敗" );
-            dic.Add( Keys.FailedToCopyFile, "複製文件失敗" );
-            dic.Add( Keys.NotInGame, "未開啟任何一個游戲存檔" );
-            dic.Add( Keys.FailedToRunViewer, $"啟動{CSLMapView.VIEWER_EXE}失敗" );
-            dic.Add( Keys.Compress, "壓縮 .cslmap (輸出 *.cslmap.gz)" );
+        private void initZh( Dictionary<StringKeys, string> dic ) {
+            dic.Add( StringKeys.Export, "輸出" );
+            dic.Add( StringKeys.ExportAndRun, "輸出后開啟" );
+            dic.Add( StringKeys.DescLabelExeOutputPath, "檢視器程式將會複製到以下路徑" );
+            dic.Add( StringKeys.DescLabelCslmapOutputPath, "地圖文件（*.cslmap）將會複製到以下路徑" );
+            dic.Add( StringKeys.FailedToExportMap, "輸出失敗" );
+            dic.Add( StringKeys.FailedToCopyFile, "複製文件失敗" );
+            dic.Add( StringKeys.NotInGame, "未開啟任何一個游戲存檔" );
+            dic.Add( StringKeys.FailedToRunViewer, $"啟動{CSLMapView.VIEWER_EXE}失敗" );
+            dic.Add( StringKeys.Compress, "壓縮 .cslmap (輸出 *.cslmap.gz)" );
         }
         /// <summary>
         /// デフォルト言語での文字列を取得します。
         /// </summary>
         /// <param name="key">文字列キー</param>
         /// <returns></returns>
-        internal string GetString(string key ) {
+        internal string GetString( StringKeys key ) {
             return GetString( Language, key );
         }
 
@@ -110,15 +118,15 @@ namespace CSLMod.CSLMapView.Modding.UI {
         /// <param name="lang">言語(en,ja)</param>
         /// <param name="key">文字列キー</param>
         /// <returns></returns>
-        internal string GetString(string lang, string key ) {
-            if(!supportedLanguages.Contains( lang )) {
+        internal string GetString( string lang, StringKeys key ) {
+            if( !supportedLanguages.Contains( lang ) ) {
                 lang = DEFAULT_LANGUAGE;
             }
-            Dictionary<string, string> dic = strings[lang];
-            if(dic.ContainsKey( key )) {
+            Dictionary<StringKeys, string> dic = strings[lang];
+            if( dic.ContainsKey( key ) ) {
                 return dic[key];
-            }else {
-                if( lang == DEFAULT_LANGUAGE) {
+            } else {
+                if( lang == DEFAULT_LANGUAGE ) {
                     return null;
                 }
                 return GetString( DEFAULT_LANGUAGE, key );
